b4dde1e12230927324627427e96163e6b740f98c
한재혁 <bears373839@gmail.com>
2021-03-09 13:38:22 +0900

    Initial version of jh001 generated by JHipster-7.0.0-beta.1

---
+++ src/test/java/net/openobject/IntegrationTest.java
@@ -0,0 +1,17 @@
+package net.openobject;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import net.openobject.Jh001App;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = Jh001App.class)
+public @interface IntegrationTest {
+}
